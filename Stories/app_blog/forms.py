9c05aea2d99787cf8cb2efd88153350190eb3817
SharfarazMahmood <sharfarazmahmood@gmail.com>
2020-12-25 14:59:31 +0600

    requirements.txt changes

--- Stories/app_blog/forms.py
+++ Stories/app_blog/forms.py
@@ -1,11 +1,9 @@
 from django import forms
 from functools import partial
-from pagedown.widgets import PagedownWidget
 from app_blog.models import Blog #, Comment
 
 
 class BlogForm(forms.ModelForm):
-    # blog_content = forms.CharField(widget=PagedownWidget())
     publish = forms.DateField( widget=forms.TextInput(attrs={'type':'Date'}) )
     class Meta():
         model = Blog
@@ -15,9 +13,3 @@ class BlogForm(forms.ModelForm):
         			'draft',
         			'publish',
         		)
-
-
-# class CommentForm(forms.ModelForm):
-#     class Meta:
-#         model = Comment
-#         fields = ('comment',)
